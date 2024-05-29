922f700114c5724f0bd486022ed214e5e253db58
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2024-05-29 15:00:59 +0530

    changed

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ app.get("/", (req, res) => {
 }); 
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`AWS EC2 Server running at http://localhost:${port}`);
 });
